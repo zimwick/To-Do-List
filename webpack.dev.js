ac2ab40a710bc468bde14ebe38ef31b56ed75d7e
zimwick <121416169+zimwick@users.noreply.github.com>
2025-07-16 12:42:24 -0600

    Build starter files

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    static: "./dist",
+    open: true,
+    hot: true,
+  },
+});
